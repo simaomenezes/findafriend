32eab7e09c6b138d4b3d9ce096cc2ec873e6cdec
Simão Neto <simaomenezesneto@gmail.com>
2024-12-06 14:23:34 +0000

    chore: Add setting project and to docker and git

---
+++ src/test/java/com/simaomenezes/findafriend/FindafriendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simaomenezes.findafriend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FindafriendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
